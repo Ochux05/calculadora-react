05d1445cf00647c53c79c7f75e5f4fa2699abee2
Juan Ochoa <122476931+Ochux05@users.noreply.github.com>
2023-01-15 23:07:54 -0500

    Add files via upload

---
+++ src v.1/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Calculator from "./Calculator"
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+
+root.render(
+    <Calculator />
+)
